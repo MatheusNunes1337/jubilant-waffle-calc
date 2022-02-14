3a2a10d0b6db54c30fc58253c39e43a50c9fcf6d
Leonardo Guimaraes Moraes <leonardo32moraes@hotmail.com>
2022-02-14 16:19:43 -0300

    feat: create add

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,9 @@
 class Calculator {
-    
+    static add(valueOne, valueTwo){
+        let sum = valueOne + valueTwo;
+        return sum;
+    }
 }
+
+const soma = Calculator.add(1,2);
+console.log(soma);
